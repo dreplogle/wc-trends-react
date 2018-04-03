ded0b26b6151c2499752c73f9fa05ab2102c41fe
Dustin.Rep <homesluice@gmail.com>
2018-04-02 17:34:26 -0700

    Added api.js and restructured app to have a Calendar component that will return dates from the wclogs guild reports endpoint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+// npm start
 import React, { Component } from 'react';
 import Chart from 'chart.js';
 import logo from './logo.svg';
 import './App.css';
+import {api_call} from './api/api.js';
 
 class App extends Component {
 	constructor(props) {
@@ -22,7 +24,6 @@ class App extends Component {
 	}
 	render() 
 	{
-		console.log(this.state.data);
 		return (
 			<div className="App">
 				<header className="App-header">
@@ -31,25 +32,60 @@ class App extends Component {
 				</header>
 				<p className="App-intro">
 					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
-				<Form onSubmit={this.handleSubmit} />
+				</p> 
+				<Calendar />
 				<Graph data={this.state.data}/>
 			</div>
 		);
 	}
 
 	handleSubmit(rank, xaxis) {
+		// Update graph
 		var data = Object.assign({}, this.state.data);
 		data.labels = xaxis;
 		data.datasets[0].data = rank;
 		this.setState({data});
-		console.log(this.state.data);
 	}
 }
 
+class Calendar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			date: [], 
+		}
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	render() {
+		const dates = this.state.date.map((date) => <Date key={date.id} value={date} />)
+		return (
+			<div className="calendar">
+				<GuildForm onSubmit={this.handleSubmit} />
+				<ul className="dates">{dates}</ul>
+			</div>
+		)
+	}
+	handleSubmit(res) {
+		this.setState({date: res});
+	}
+}
 
+class Date extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: null,
+		};
+	}
 
-class Form extends Component {
+	render() {
+		return (
+			<li>{this.props.value.title}</li>
+		)
+	}
+}
+
+class UserForm extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
@@ -70,38 +106,23 @@ class Form extends Component {
 	}
 	
 	handleSubmit(event) {
-		console.log("hi");
-		fetch("http://localhost:4000/report/" + this.state.username + "/" + this.state.realm + "/" + this.state.region)
-		.then(res => res.json())
-		.then(
-			(res) => {
-				let rank = [];
-				let xaxis = [];
-				let x = 1;
-				let key = "";
-				for(key in res) {
-					if(res[key].difficulty == 4) {
-						let num = "";
-						// console.log(res[key]['specs']['0']['data']['1'].percent);
-						for(num in res[key]['specs']['0']['data']) {
-							// console.log(num);
-							// console.log(res[key]['specs']['0']['data'][num].percent)
-							rank.push(res[key]['specs']['0']['data'][num].percent);
-							xaxis.push(x);
-						}
-						x++;
+		api_call(this.state.username + "/" + this.state.realm + "/" + this.state.region, (res) => {
+			let rank = [];
+			let xaxis = [];
+			let x = 1;
+			let key = "";
+			for(key in res) {
+				if(res[key].difficulty === 4) {
+					let num = "";
+					for(num in res[key]['specs']['0']['data']) {
+						rank.push(res[key]['specs']['0']['data'][num].percent);
+						xaxis.push(x);
 					}
+					x++;
 				}
-				this.props.onSubmit(rank, xaxis);
-			},
-			// Note: it's important to handle errors here
-			// instead of a catch() block so that we don't swallow
-			// exceptions from actual bugs in components.
-			(error) => {
-				console.log(error);
 			}
-		);
-
+			this.props.onSubmit(rank, xaxis);
+		})
 		event.preventDefault();
 	}
 	render () {
@@ -122,6 +143,49 @@ class Form extends Component {
 	}
 }
 
+class GuildForm extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			guild: 'smol pupper',
+			realm: 'proudmoore',
+			region: 'us'
+		}
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+	handleChange(event) {
+		const target = event.target;
+		const name = target.name;
+		const value = target.value;
+		this.setState({
+			[name]: value
+		});
+	}
+	
+	handleSubmit(event) {
+		api_call("guild/" + this.state.guild + "/" + this.state.realm + "/" + this.state.region, (res) => {
+			this.props.onSubmit(res);
+		})
+		event.preventDefault();
+	}
+	render () {
+		return ( 
+			<form onSubmit={this.handleSubmit}>
+				<label>Guild Name
+					<input type="text" name="guild" value={this.state.guild} onChange={this.handleChange} />
+				</label>
+				<label>Realm
+					<input type="text" name="realm" value={this.state.realm} onChange={this.handleChange} />
+				</label>
+				<label>Region
+					<input type="text" name="region" value={this.state.region} onChange={this.handleChange} />
+				</label>
+				<input type="submit" value="Submit" />
+			</form>
+		)
+	}
+}
 
 class Graph extends Component {
 	componentDidMount() {
---
+++ src/api/api.js
@@ -0,0 +1,20 @@
+const api_root = "http://localhost:4000/report/"
+
+export function api_call(req, handler) {
+    // console.log(api_root + req);
+    fetch(api_root + req)
+    .then(res => res.json())
+    .then(
+        (res) => {
+            handler(res);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+            console.log(error);
+        }
+    );
+}
+
+// export api_call;
